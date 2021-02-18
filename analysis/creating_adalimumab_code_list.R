6702b5a878dcdb26e2dbd3d19f4a1c643cf0c5ca
AnnaB45 <36034515+AnnaB45@users.noreply.github.com>
2021-02-18 14:47:12 +0000

    Updated codelist, project.yaml and study definition
    Putting together a study definition to illustrate how the high cost drugs data can be used

---
+++ analysis/creating_adalimumab_code_list.R
@@ -0,0 +1,20 @@
+## simple script to create new csv with unique drug names from the high cost drugs dataset
+
+install.packages("tidyverse")
+install.packages("here")
+library(tidyverse)
+
+drug_list <- read.csv(file = here::here("GitHub", "highcostdrugs-research","released-output", "drug_name_unique.csv")) %>%
+  select("DrugName")
+
+colnames(drug_names) <- column_names
+
+code_list_adalimumab <- drug_list %>%
+  mutate(Check_adalimumab = str_detect(DrugName,"adalimumab"),
+         Check_ADALIMUMAB = str_detect(DrugName,"ADALIMUMAB"),
+         Check_Adalimumab = str_detect(DrugName,"Adalimumab"),
+         In_Group = ifelse((Check_Adalimumab == TRUE | Check_adalimumab == TRUE | Check_ADALIMUMAB == TRUE), TRUE, FALSE)) %>%
+  filter(In_Group == TRUE) %>%
+  select(DrugName)
+
+write.csv(code_list_adalimumab, file = here::here("GitHub", "highcostdrugs-research","codelists", "code_list_adalimumab.csv"))
---
+++ analysis/study_definition_adalimumab.py
@@ -0,0 +1,65 @@
+
+# Import functions
+
+from cohortextractor import (
+    StudyDefinition, 
+    patients, 
+    codelist_from_csv
+)
+
+# Import codelists
+
+adalimumab_codes = codelist_from_csv(
+    "codelists/code_list_adalimumab.csv", system="highcostdrugs", column="DrugName"
+)
+
+# Specifiy study defeinition
+
+study = StudyDefinition(
+    # Configure the expectations framework
+    default_expectations={
+        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "rate": "exponential_increase",
+    },
+    # This line defines the study population
+    population=patients.registered_with_one_practice_between(
+        "2018-04-01", "2020-03-01"
+    ),
+
+    # https://github.com/opensafely/risk-factors-research/issues/49
+    age=patients.age_as_of(
+        "2020-03-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+    
+    # https://github.com/opensafely/risk-factors-research/issues/46
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+
+    # https://github.com/opensafely/risk-factors-research/issues/44
+    stp=patients.registered_practice_as_of(
+        "2020-03-01",
+        returning="stp_code",
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"STP1": 0.5, "STP2": 0.5}},
+        },
+    ),
+
+    #https://github.com/opensafely-core/cohort-extractor/blob/819b1ca256d4c0f3ff2fca68389cc846417a021c/cohortextractor/patients.py#L1731-L1765
+    prescribed_adalimumab=patients.with_high_cost_drugs(
+                drug_name_matches= adalimumab_codes,
+                on_or_before="2020-03-31",
+                find_first_match_in_period=True,
+                returning="binary flag",
+                date_format="YYYY-MM",
+                return_expectations=None,
+            )
+)
