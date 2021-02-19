561ab2d1255a787fd1de4a1362074095a09a8971
AnnaB45 <36034515+AnnaB45@users.noreply.github.com>
2021-02-19 12:53:02 +0000

    Updates to codelist
    Various changes whilst trying to update codelist

--- analysis/creating_adalimumab_code_list.R
+++ analysis/creating_adalimumab_code_list.R
@@ -1,7 +1,7 @@
 ## simple script to create new csv with unique drug names from the high cost drugs dataset
 
-install.packages("tidyverse")
-install.packages("here")
+# install.packages("tidyverse")
+# install.packages("here")
 library(tidyverse)
 
 drug_list <- read.csv(file = here::here("GitHub", "highcostdrugs-research","released-output", "drug_name_unique.csv")) %>%
@@ -17,4 +17,5 @@ code_list_adalimumab <- drug_list %>%
   filter(In_Group == TRUE) %>%
   select(DrugName)
 
-write.csv(code_list_adalimumab, file = here::here("GitHub", "highcostdrugs-research","codelists", "code_list_adalimumab.csv"))
+write.table(code_list_adalimumab, file = here::here("GitHub", "highcostdrugs-research","codelists", "code_list_adalimumab.csv"),
+            row.names = FALSE, col.names = FALSE)
